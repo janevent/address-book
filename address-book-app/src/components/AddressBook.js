0134e977d28576bf769217346bc63a4c83b55618
janeventer <janeanneventer@gmail.com>
2021-02-16 14:26:58 -0800

    import and render SearchBar component

--- address-book-app/src/components/AddressBook.js
+++ address-book-app/src/components/AddressBook.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 export default class AddressBook{
 
     render(){
         return(
-            <div></div>
+            <div>
+                <SearchBar/>
+            </div>
         )
     }
 }
