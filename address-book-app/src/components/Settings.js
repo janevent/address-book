1d5062df99723517187e33ffd72c03de03f94cb6
janeventer <janeanneventer@gmail.com>
2021-02-16 14:36:47 -0800

    write a Settings component

---
+++ address-book-app/src/components/Settings.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Settings extends Component {
+    render(){
+        <div className="Settings">
+            <h2>Settings</h2>
+            <h2>Pick A Country</h2>
+        </div>
+    }
+}
