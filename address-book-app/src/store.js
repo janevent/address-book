351e95651faeaf276d899545819f1ed7ef3421d6
janeventer <janeanneventer@gmail.com>
2021-02-16 17:17:28 -0800

    import applyMiddleware and thunk, and utilize them

--- address-book-app/src/store.js
+++ address-book-app/src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'store';
+import { createStore, applyMiddleware } from 'store';
 import appReducer from 'reducer.js';
+import thunk from 'redux-thunk';
 
-const store = createStore(appReducer)
+
+const store = createStore(appReducer, applyMiddleware(thunk))
 
 export default store;
