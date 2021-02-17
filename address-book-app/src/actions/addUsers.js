be890e383ed938410891d31cf26e3d69937b74b3
janeventer <janeanneventer@gmail.com>
2021-02-16 23:56:41 -0800

    write fetchAndAddUsers to dispatch the addUsers action

--- address-book-app/src/actions/addUsers.js
+++ address-book-app/src/actions/addUsers.js
@@ -5,4 +5,20 @@ const addUsers = (users) => {
     }
 }
 
-export default addUsers
+const fetchAndAddUsers = (users) => {
+    return (dispatch) => {
+        fetch('https://randomuser.me/api/?results=50')
+        .then(response => response.json())
+        .then(data => {
+            if(data.results){
+                dispatch(addUsers(data.results))
+            }else if(data.error){
+                console.log(error)
+            }
+        })
+        .catch(error => {
+            console.log("error", error)
+        })
+    }
+}
+export default fetchAndAddUsers
