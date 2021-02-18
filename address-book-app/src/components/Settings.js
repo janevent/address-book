a870aa2b1c4fad5574aa046bb2b03515083ac134
janeventer <janeanneventer@gmail.com>
2021-02-18 15:04:18 -0800

    set state to have a key of checkboxes pointing to an object with the key value pairs of nationality and false

--- address-book-app/src/components/Settings.js
+++ address-book-app/src/components/Settings.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import Button from './Button.js';
 
 export default class Settings extends Component {
+    
+    OPTIONS = ['AU', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IE', 'IR', 'NO', 'NL', 'NZ', 'TR', 'US'];
+
+        state = {
+            checkboxes: OPTIONS.reduce(
+                (options, option) => ({
+                    ...options, 
+                    [option]: false
+                }),
+                {}
+            )
+        }
+    
     render(){
         return(
             <div className="Settings">
