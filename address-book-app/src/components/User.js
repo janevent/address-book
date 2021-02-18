725a691a209fe6327e0c62cec3adff5d49b9d2e3
janeventer <janeanneventer@gmail.com>
2021-02-17 17:51:37 -0800

    refactor for easy read, and display full name and email

--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -4,10 +4,13 @@ import '../styles/User.css';
 export default class User extends React.Component{
 
     render(){
+        let {name, picture, email} = this.props.user
         return(
             <div className="User">
-            <img src={this.props.user.picture.large}/>
-                <h1>{this.props.user.name.first}</h1>
+            <img src={picture.large}/>
+                <h1>{name.title} {name.first} {name.last}</h1>
+                <h3>Username: {name.username}</h3>
+                <p>Email: {email}</p>
             </div>
 
         )
