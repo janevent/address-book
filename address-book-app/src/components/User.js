e0a7813952c3b2f7d46f86ea8aaef9b2a6d4f36e
janeventer <janeanneventer@gmail.com>
2021-02-17 17:42:12 -0800

    display users photo

--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import '../styles/User.css';
 
 export default class User extends React.Component{
 
     render(){
         return(
-            <div className="user">
+            <div className="User">
+            <img src={this.props.user.picture.large}/>
                 <h1>{this.props.user.name.first}</h1>
             </div>
 
