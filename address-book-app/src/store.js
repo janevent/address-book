501b8498b720b19fa3b564eaccd0462774403ad9
janeventer <janeanneventer@gmail.com>
2021-02-17 16:43:30 -0800

    use combine reducers to set global state to an array of users

--- address-book-app/src/store.js
+++ address-book-app/src/store.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import appReducer from './reducer.js';
 import thunk from 'redux-thunk';
 
+const usersReducer = combineReducers({
+    users: appReducer
+})
 
-const store = createStore(appReducer, applyMiddleware(thunk))
+const store = createStore(usersReducer, applyMiddleware(thunk))
 
 export default store;
