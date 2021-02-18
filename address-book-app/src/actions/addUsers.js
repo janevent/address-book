d79baa5755d34d9fa7a2f20bd98b992b679799e1
janeventer <janeanneventer@gmail.com>
2021-02-17 17:57:18 -0800

    also fetch login data so as to display username

--- address-book-app/src/actions/addUsers.js
+++ address-book-app/src/actions/addUsers.js
@@ -7,11 +7,13 @@ const addUsers = (users) => {
 
 const fetchAndAddUsers = (users) => {
     return (dispatch) => {
-        fetch('https://randomuser.me/api/?results=50')
+        fetch('https://randomuser.me/api/?results=50&inc=name,email,picture,id,login')
         .then(response => response.json())
         .then(data => {
             if(data.results){
+                console.log(data.results)
                 dispatch(addUsers(data.results))
+                
             }else if(data.error){
                 console.log(data.error)
             }
--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -4,12 +4,12 @@ import '../styles/User.css';
 export default class User extends React.Component{
 
     render(){
-        let {name, picture, email} = this.props.user
+        let {name, picture, email, login} = this.props.user
         return(
             <div className="User">
             <img src={picture.large}/>
                 <h1>{name.title} {name.first} {name.last}</h1>
-                <h3>Username: {name.username}</h3>
+                <h3>Username: {login.username}</h3>
                 <p>Email: {email}</p>
             </div>
 
