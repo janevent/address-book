f4efa77f9b139f39b05b48cf6d9c9c93bddd2199
janeventer <janeanneventer@gmail.com>
2021-02-18 13:34:34 -0800

    combine reducer with settingsReducer

--- address-book-app/src/store.js
+++ address-book-app/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import appReducer from './reducer.js';
+import settingsReducer from './settingsReducer.js';
 import thunk from 'redux-thunk';
 
 const usersReducer = combineReducers({
-    users: appReducer
+    users: appReducer,
+    nationalities: settingsReducer
 })
 
 const store = createStore(usersReducer, applyMiddleware(thunk))
