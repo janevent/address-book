f395ddf4de2dde5ae4aabe99c7cdd04f0cf93a7a
janeventer <janeanneventer@gmail.com>
2021-02-19 15:36:12 -0800

    write an action creator toclear and repopulate the users in global store

---
+++ address-book-app/src/actions/addFirstUsers.js
@@ -0,0 +1,28 @@
+const addFirstUsers = (users) => {
+    return {
+        type: 'ADD_USERS',
+        users: users
+    }
+}
+
+const fetchAndAddFirstUsers = ( n) => {
+    return (dispatch) => {
+        console.log(n)
+        let url = (n===undefined ) ? 'https://randomuser.me/api/?results=50&inc=name,email,picture,id,login,nat' :
+        `https://randomuser.me/api/?results=50&inc=name,email,picture,id,login,nat${n}`
+        console.log("url", url)
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if(data.results){
+                console.log(data.results)
+                dispatch(addFirstUsers(data.results))
+                
+            }else if(data.error){
+                console.log(data.error)
+            }
+        })
+        .catch(console.log)
+    }
+}
+export default fetchAndAddFirstUsers
