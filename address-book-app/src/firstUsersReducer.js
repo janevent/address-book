952b005b001443ccac5ecfbb22aba7257bcc973a
janeventer <janeanneventer@gmail.com>
2021-02-19 15:51:50 -0800

    delete unnecessary reducer

--- address-book-app/src/firstUsersReducer.js
+++
@@ -1,8 +0,0 @@
-export default function firstUsersReducer(state=[], action){
-    switch(action.type){
-        case 'ADD_FIRST_USERS':
-            return [...action.users]
-        default:
-            return state
-    }
-}
