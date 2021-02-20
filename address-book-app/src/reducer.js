0f0093571973dcdce62a09fc93b41f7e9ea50321
janeventer <janeanneventer@gmail.com>
2021-02-19 18:34:39 -0800

    add case for 'SET_FILTERED_USERS'

--- address-book-app/src/reducer.js
+++ address-book-app/src/reducer.js
@@ -6,6 +6,8 @@ export default function appReducer(state=[], action){
         case 'ADD_FIRST_USERS':
             console.log('ADD_FIRST_USERS', action.users)
             return [...action.users]
+        case 'SET_FILTERED_USERS':
+            return [...action.users]
         default:
             return state
     }
