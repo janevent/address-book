a7b41520f81e46cf8e6978eee896820858acdd36
janeventer <janeanneventer@gmail.com>
2021-02-21 23:32:44 -0800

    display user info in centered white block when User component clicked

--- address-book-app/src/components/Modal.js
+++ address-book-app/src/components/Modal.js
@@ -1,13 +1,29 @@
-import '../styles/Modal.css'
+import React from 'react';
+import '../styles/Modal.css';
 
 const Modal = ({handleClose, show, info}) => {
-    const showHideClassName = show ? "Modal display-block" : "Modal display-none";
-
+    let showHideClassName = show ? "Modal display-block" : "Modal display-none";
+    // <p>City: {info.location.city}</p>
+    //         <p>State: {info.location.state}</p>
+    //         <p>Postal Code: {info.location.postalcode}</p>
+    //         <p>Phone: {info.phone}</p>
+    //         <p>Cell: {info.cell}</p>
+    const handleClick = () => {
+        showHideClassName = "Modal display-none"
+        console.log(showHideClassName)
+    }
     return (
         <div className={showHideClassName}>
-            <button type="button" onClick={handleClose}>
-                 Close
-            </button>
+            <section className="main">
+                <h3>Address</h3>
+                <p>{info.location.street.number} {info.location.street.name}</p>
+                <p>{info.location.city}, {info.location.state}</p>
+                <p>{info.location.postcode}</p>
+                <p>{info.phone}</p><p>{info.cell}</p>
+                <button type="button" onClick={handleClick}>
+                    Close
+                </button>
+            </section>
         </div>
     )
 }
