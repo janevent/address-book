04b836469598e2c416077b44cfb623ee37e8c2b8
janeventer <janeanneventer@gmail.com>
2021-02-21 19:18:10 -0800

    fire onClickUser when user clicks User component, set state to {show: false} and change it to {show:true} when User is clicked

--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 import '../styles/User.css';
 
 export default class User extends React.Component{
-
+    constructor(){
+        super()
+        this.state = {
+            show: false
+        }
+    }
+    onClickUser = () => {
+        console.log(this.props.user.location, this.props.user.cell, this.props.user.phone)
+        this.setState({
+            show: true
+        })
+    }
     render(){
-        let {name, picture, email, login, nat} = this.props.user
+        let {name, picture, email, login, location, cell, phone, nat} = this.props.user
         return(
-            <div className="User">
+            <div className="User" onClick={this.onClickUser}>
                 <img className="image" src={picture.large}/>
                 <h3>{name.title} {name.first} {name.last}</h3>
                 <h3>Username: {login.username}  Nat: {nat}</h3>
