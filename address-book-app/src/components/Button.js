96395ff5edaec66f0d9065875adf93331629f386
janeventer <janeanneventer@gmail.com>
2021-02-21 17:37:39 -0800

    wrap in router

--- address-book-app/src/components/Button.js
+++ address-book-app/src/components/Button.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default class Button extends React.Component {
 
     render(){
         return (
+            <Router>
             <Link to="/">
                 <button type="button">Back To Address Book</button>
             </Link>
+            </Router>
         )
     }
 }
