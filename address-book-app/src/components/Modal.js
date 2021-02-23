f5378e07019bb11e2601f8ebc71e3429ed76ecf5
janeventer <janeanneventer@gmail.com>
2021-02-22 17:10:37 -0800

    created a Modal component (not used now)

--- address-book-app/src/components/Modal.js
+++ address-book-app/src/components/Modal.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import '../styles/Modal.css';
 
-const Modal = ({handleClose, show, info}) => {
-    let showHideClassName = show ? "Modal display-block" : "Modal display-none";
-    // <p>City: {info.location.city}</p>
-    //         <p>State: {info.location.state}</p>
-    //         <p>Postal Code: {info.location.postalcode}</p>
-    //         <p>Phone: {info.phone}</p>
-    //         <p>Cell: {info.cell}</p>
-    const handleClick = () => {
-        showHideClassName = "Modal display-none"
-        console.log(showHideClassName)
-    }
+class Modal extends React.Component{
+   
+    
+    render(){
+        let showHideClassName = this.props.show ? "Modal display-block" : "Modal display-none";
     return (
         <div className={showHideClassName}>
             <section className="main">
                 <h3>Address</h3>
-                <p>{info.location.street.number} {info.location.street.name}</p>
-                <p>{info.location.city}, {info.location.state}</p>
-                <p>{info.location.postcode}</p>
-                <p>{info.phone}</p><p>{info.cell}</p>
-                <button type="button" onClick={handleClick}>
+                <p>{this.props.info.location.street.number} {this.props.info.location.street.name}</p>
+                <p>{this.props.info.location.city}, {this.props.info.location.state}</p>
+                <p>{this.props.info.location.postcode}</p>
+                <p>{this.props.info.phone}</p><p>{this.props.info.cell}</p>
+                <button type="button" onClick={this.props.handleClose}>
                     Close
                 </button>
             </section>
         </div>
     )
+    }
 }
 
 export default Modal;
