d278b12397b19363d4cd4fe4c6b3a4bf0e4cea3f
janeventer <janeanneventer@gmail.com>
2021-02-23 00:46:59 -0800

    delete extra spacing

--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -6,17 +6,13 @@ export default class User extends React.Component{
 
     render(){
         let {name, picture, email, login, nat} = this.props.user
-        return(
-            
+        return(           
             <div className="User">
                 <img className="image" src={picture.large}/>
                 <h3>{name.title} {name.first} {name.last}</h3>
                 <h3>Username: {login.username}  Nat: {nat}</h3>
                 <p>Email: {email}</p>               
-            </div>
-            
-            
-
+            </div>            
         )
     }
 }
