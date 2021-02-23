ecbe244cbeffcc27ded3e270c613d851992b311f
janeventer <janeanneventer@gmail.com>
2021-02-22 23:11:56 -0800

    take out Router

--- address-book-app/src/components/Button.js
+++ address-book-app/src/components/Button.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 export default class Button extends React.Component {
 
     render(){
-        return (
-            <Router>
+        return (            
             <Link to="/">
                 <button type="button">Back To Address Book</button>
             </Link>
-            </Router>
         )
     }
 }
