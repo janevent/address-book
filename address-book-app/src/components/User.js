1ccc5fdbd30d6fedb0003306db0c1301e58c917a
janeventer <janeanneventer@gmail.com>
2021-02-22 23:44:42 -0800

    delete no longer used code

--- address-book-app/src/components/User.js
+++ address-book-app/src/components/User.js
@@ -3,27 +3,6 @@ import '../styles/User.css';
 
 
 export default class User extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            Show: false
-        }
-       // this.onClickUser = this.onClickUser.bind(this)
-        //this.exitUser = this.exitUser.bind(this)
-    }
-    onClickUser = () => {
-        //document.querySelector('.Modal').classList.remove("display-none")
-        //console.log(this.props.user.location, this.props.user.cell, this.props.user.phone)
-        this.setState({ Show: true })       
-    }
-
-    exitUser = () => {
-        console.log(this.state)
-        this.setState({Show: false})
-        //document.querySelector('.Modal').classList.add("display-none");
-        //console.log("exit", this.state)
-        setTimeout(() => { console.log(this.state) }, 10000)
-    }
 
     render(){
         let {name, picture, email, login, nat} = this.props.user
