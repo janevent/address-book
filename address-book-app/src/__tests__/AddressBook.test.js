d2e85f66933edcb6c7025c32560f25e64259147a
janeventer <janeanneventer@gmail.com>
2021-02-23 00:28:33 -0800

    clean up code

--- address-book-app/src/__tests__/AddressBook.test.js
+++ address-book-app/src/__tests__/AddressBook.test.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-//const renderer = require('react-test-renderer')
-import settingsReducer from '../settingsReducer';
+import settingsReducer from '../settingsReducer'
 import appReducer from '../reducer';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import AddressBook from '../components/AddressBook';
 import thunk from 'redux-thunk'
-//import configureMockStore from 'redux-mock-store';
-//const middlewares = [thunk]
-//const mockStore = configureMockStore(middlewares)
-
-
 
 function createTestStore() {
     const store = createStore(
@@ -42,26 +35,3 @@ describe('renders AddressBook', () => {
     })
 })
  
-// const mockStore = configureStore({});
-
-// describe('renders users from store', () => {
-//     let store;
-//     let component;
-
-//     beforeEach(() => {
-//         store = mockStore({
-//         myState: {
-//             users: [{name: {title: 'Mrs', first: 'Geraldo', last: 'Thorp'}, login: {}, email: 'pandas@example.com', nat: 'CA'}],
-//             nationalities: []},
-//         });
-//         component = renderer.create(
-//             <Provider store={store}>
-//                 <AddressBook />
-//             </Provider>
-//         );
-//     });
-
-//     test('renders users', () => {
-//         //expect(component.toJSON()).toMatchSnapshot();
-//     })
-// })
