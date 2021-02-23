9222cefe4435c162210e28b81415197278696178
janeventer <janeanneventer@gmail.com>
2021-02-22 17:06:31 -0800

    access actual store to implement test suite

--- address-book-app/src/__tests__/AddressBook.test.js
+++ address-book-app/src/__tests__/AddressBook.test.js
@@ -3,31 +3,65 @@ import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 //const renderer = require('react-test-renderer')
-
+import settingsReducer from '../settingsReducer';
+import appReducer from '../reducer';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import AddressBook from '../components/AddressBook';
+import thunk from 'redux-thunk'
+//import configureMockStore from 'redux-mock-store';
+//const middlewares = [thunk]
+//const mockStore = configureMockStore(middlewares)
 
-import configureStore from 'redux-mock-store';
- 
-const mockStore = configureStore([]);
 
-describe('renders users from store', () => {
-    let store;
-    let component;
 
+function createTestStore() {
+    const store = createStore(
+      combineReducers({
+        user: appReducer,
+        settings: settingsReducer,
+      }),
+      applyMiddleware(thunk)
+    );
+    return store;
+}
+
+let store;
+
+describe('renders AddressBook', () => {
     beforeEach(() => {
-        store = mockStore({
-        myState: {
-            users: [{name: {title: 'Mrs', first: 'Geraldo', last: 'Thorp'}, login: {}, email: 'pandas@example.com', nat: 'CA'}],
-            nationalities: []},
-        });
-        component = renderer.create(
-            <Provider store={store}>
-                <AddressBook />
-            </Provider>
-        );
-    });
+        store = createTestStore()
+    })
 
-    test('renders users', () => {
-        //expect(component.toJSON()).toMatchSnapshot();
+    it('should display users', () => {
+        const {find} = render(
+            <Provider store={store}>
+                <AddressBook/>
+            </Provider>
+        )
+        expect(screen.getByRole('textbox')).toBeInTheDocument();
     })
 })
+ 
+// const mockStore = configureStore({});
+
+// describe('renders users from store', () => {
+//     let store;
+//     let component;
+
+//     beforeEach(() => {
+//         store = mockStore({
+//         myState: {
+//             users: [{name: {title: 'Mrs', first: 'Geraldo', last: 'Thorp'}, login: {}, email: 'pandas@example.com', nat: 'CA'}],
+//             nationalities: []},
+//         });
+//         component = renderer.create(
+//             <Provider store={store}>
+//                 <AddressBook />
+//             </Provider>
+//         );
+//     });
+
+//     test('renders users', () => {
+//         //expect(component.toJSON()).toMatchSnapshot();
+//     })
+// })
